f1c5a3f1caca960c00ba2802cae4bf6a55d22acc
Rafaela Ilarri <r_ilarri@mail.fhsu.edu>
2024-03-19 01:01:22 +0100

    Updated delete quotes

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -1,12 +1,9 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 // CORS
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: DELETE');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 // Include necessary files
 include_once '../../config/Database.php';
@@ -19,25 +16,28 @@ $db = $database->connect();
 // Create Quote object
 $quote = new Quote($db);
 
-echo "passes here";
-// Get quote id from URL
-$quote->id = isset($_GET['id']) ? $_GET['id'] : die();
-echo "passes here 2";
+// Get quote id from request body
+$data = json_decode(file_get_contents("php://input"));
 
-// Check if quote exists before trying to delete 
-if (!$quote->quoteExists()) 
-{
-    echo json_encode(array('message' => 'No Quotes Found'));
-} else 
-{
-    // Delete quote
-    if ($quote->delete()) {
-        echo json_encode(
-            array('id' => $quote->id)
-        );
+// Check if id is provided in the request body
+if (!empty($data->id)) {
+    // Set id property of quote object
+    $quote->id = $data->id;
+
+    // Check if quote exists before trying to delete 
+    if ($quote->quoteExists()) {
+        // Delete quote
+        if ($quote->delete()) {
+            // Return the id of the deleted quote in JSON format
+            echo json_encode(array('id' => $quote->id));
+        } else {
+            echo json_encode(array('message' => 'Quote not deleted'));
+        }
     } else {
-        echo json_encode(array('message' => 'Quote not deleted'));
+        echo json_encode(array('message' => 'No Quotes Found'));
     }
+} else {
+    // If id is not provided in the request body
+    echo json_encode(array('message' => 'Missing id in request body'));
 }
-
 ?>
